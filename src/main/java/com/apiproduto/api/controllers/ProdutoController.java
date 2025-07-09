e1eb91f6728be21e5d00c58baa581364244df228
Anderson Lima <arolima546@gmail.com>
2025-07-09 19:39:09 -0300

    first commit

---
+++ src/main/java/com/apiproduto/api/controllers/ProdutoController.java
@@ -0,0 +1,25 @@
+package com.apiproduto.api.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.apiproduto.api.models.ProdutoRepository;
+
+@Controller //trabalhando com front e back se usa o controlle não restcontroller
+public class ProdutoController {
+    
+    @Autowired
+    private ProdutoRepository repository;
+
+    @GetMapping("/listagem")
+    public String listarProdutos(){
+        return "listar";
+    }
+    @GetMapping("/cadastro")
+    public String cadastrarProdutos(){
+        return "cadastrar";
+    }
+
+
+
+}
